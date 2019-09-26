cc09b70656e5b363a5e20e4e77cbb6219343b92d
Evans Ehiorobo <ehioroboevans@gmail.com>
2019-09-26 16:18:16 +0100

    Added models test

--- my_blog_app/blog/blogsite/models.py
+++ my_blog_app/blog/blogsite/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True, max_length=255)
+    author = models.TextField()
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super(Post, self).save(*args, **kwargs)
